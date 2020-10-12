b6a6224738c29e5a4a79e1200f0c1198ba6c9ff7
Alex A <alex.alanizaa@gmail.com>
2020-10-12 15:10:04 -0500

    Test Commit, It Changed a Lot for some reason

---
+++ MainActivity.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace RomanNumerals
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private TextView calcText;
+        private string[] numbers = new string[2];
+        private string @operator;
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            //FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            //fab.Click += FabOnClick;
+
+            calcText = FindViewById<TextView>(Resource.Id.calcTextView);
+        }
+
+        [Java.Interop.Export("ButtonClick")]
+        public void ButtonClick(View v)
+        {
+            Button button = (Button)v;
+            if ("0123456789.".Contains(button.Text))
+            {
+                AddDigitOrDecimalPoint(button.Text);
+            }
+            else if ("*/+-".Contains(button.Text))
+            {
+                AddOperator(button.Text);
+            }
+            else if ("=" ==  button.Text)
+            {
+                Calculate();
+            }
+            else
+            {
+                Erase();
+            }
+        }
+
+        private void Erase()
+        {
+            numbers[0] = numbers[1] = null;
+            @operator = null;
+            UpdateCalculatorText();
+        }
+
+        private void Calculate(string newOperator = null)
+        {
+            double? result = null;
+            double? first = numbers[0] == null ? null : (double?)double.Parse(numbers[0]);
+            double? second = numbers[1] == null ? null : (double?)double.Parse(numbers[1]);
+
+            switch (@operator) {
+                case "/":
+                    result = first / second;
+                    break;
+                case "*":
+                    result = first * second;
+                    break;
+                case "+":
+                    result = first + second;
+                    break;
+                case "-":
+                    result = first - second;
+                    break;
+            }
+
+            if(result != null)
+            {
+                numbers[0] = result.ToString();
+                @operator = newOperator;
+                numbers[1] = null;
+                UpdateCalculatorText();
+            }
+        }
+
+        private void AddOperator(string val)
+        {
+            if(numbers[1] != null)
+            {
+                Calculate(val);
+                return;
+            }
+
+            @operator = val;
+
+            UpdateCalculatorText();
+        }
+
+        private void AddDigitOrDecimalPoint(string val)
+        {
+            int index = (@operator == null ? 0 : 1);
+            
+            if(val == "." && numbers[index].Contains("."))
+            {
+                return;
+            }
+            numbers[index] += val;
+
+            UpdateCalculatorText();
+
+
+        }
+
+        private void UpdateCalculatorText() => calcText.Text = $"{numbers[0]} {@operator} {numbers[1]}";
+
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            View view = (View) sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+
+	}
+}
